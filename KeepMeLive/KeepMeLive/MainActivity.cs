b768a7837950c0fb0c6ad28ee9979077a7bf18e4
Swapnil <Swapnil@SWASTIK>
2016-05-30 19:21:59 +0530

    30th May 2016 commit

--- KeepMeLive/KeepMeLive/MainActivity.cs
+++ KeepMeLive/KeepMeLive/MainActivity.cs
@@ -11,20 +11,13 @@ namespace KeepMeLive
     [Activity(Label = "KeepMeLive", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
-        int count = 1;
-
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
-
-            // Get our button from the layout resource,
-            // and attach an event to it
-            Button button = FindViewById<Button>(Resource.Id.MyButton);
-
-            button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+           
         }
     }
 }
